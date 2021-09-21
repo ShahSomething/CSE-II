f7a72043df42b00767cbfc43a2b2ed31675818fe
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-09-22 01:19:00 +0500

    Add files via upload

---
+++ 5th Semester/System Programming Lab/Lab 03/Task2/ProcessFan.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	int x;
+	for (int i=0;i<atoi(argv[1]);i++)
+	{
+		x= fork();
+		if(x==0)
+			break;
+	}
+	for(int i=0;i<atoi(argv[1]);i++)
+		wait(NULL);
+	printf("Pid: %d, Parent id: %d\n",getpid(),getppid());
+	sleep(15);
+	
+}
