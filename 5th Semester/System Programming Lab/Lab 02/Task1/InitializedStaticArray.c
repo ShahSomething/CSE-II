0cc5549ccc71997afbb4e142a5ed930cd4aa2e71
Shah Raza <63047096+ShahSomething@users.noreply.github.com>
2021-09-22 01:03:56 +0500

    Add files via upload

---
+++ 5th Semester/System Programming Lab/Lab 02/Task1/InitializedStaticArray.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+static int Array[1000]={7,3,1};
+
+int main()
+{
+	printf("Initialized Static Array");
+	return 0;
+}
+
+
---
+++ 5th Semester/System Programming Lab/Lab 02/Task1/UninitializedStaticArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+static int Array[1000];
+
+int main()
+{
+	printf("Uninitialized Static Array");
+	return 0;
+}
+
